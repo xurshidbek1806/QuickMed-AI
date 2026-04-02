2d038331bb48830c9e03ae6d9ca6a5b6617cdd06
Javohirbek <safarovjavohirbek3@gmail.com>
2026-04-02 14:48:02 +0500

    Remove unused base URL from ApiConfig

--- mobile/lib/config/api_config.dart
+++ mobile/lib/config/api_config.dart
@@ -1,6 +1,5 @@
 class ApiConfig {
   static const String baseUrl = 'http://ai.sysmasters.uz';
-  static const String baseUrl1 = 'http://192.168.213.3:8082';
   static const String chatInit = '/api/chat/init';
   static const String chatMessage = '/api/chat/message';
 }
